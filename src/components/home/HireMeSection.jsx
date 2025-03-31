a7ffd2a19844856fe50b42a8122ed557a9d02d69
jungyoonn <sophia7625@naver.com>
2025-03-31 14:20:46 +0900

    링크 안 먹는 이슈 수정, 샘플 멘트 수정

--- src/components/home/HireMeSection.jsx
+++ src/components/home/HireMeSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import hireMeImg from '../../img/slider_profile_img1.png';
 
 const HireMeSection = () => {
   return (
@@ -8,15 +8,15 @@ const HireMeSection = () => {
         <div className="row justify-content-between">
           <div className="col-md-8 col-lg-8 d-flex align-items-center">
             <div className="w-100 py-4">
-              <h2>Have a project on your mind.</h2>
-              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly.</p>
+              <h2>기업의 성장과 더불어 함께 발전해 나가고 싶습니다!</h2>
+              <p>열정과 잠재력을 갖춘 풀스택 개발자로서 새로운 도전에 참여하고 싶습니다. 6개월의 실무 경험과 교육 과정에서 쌓은 기술력을 바탕으로, 프로젝트에 신선한 관점과 배움에 대한 의지를 더하겠습니다. 성장하는 팀의 일원이 되어 함께 가치 있는 경험을 쌓고 싶습니다.</p>
               <p className="mb-0">
-                <Link to="#contact-section" className="btn btn-white py-3 px-4">Contact me</Link>
+              <a href="#contact-section" className="btn btn-white py-3 px-4">Contact me</a>
               </p>
             </div>
           </div>
           <div className="col-md-4 col-lg-4 d-flex align-items-end">
-            <img src="images/author.png" className="img-fluid" alt="Author" />
+            <img src={hireMeImg} className="img-fluid" alt="Author" />
           </div>
         </div>
       </div>
