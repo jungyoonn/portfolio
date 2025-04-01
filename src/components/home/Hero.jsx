ea19e4ff63a2680ba999534d861db6c9523a0538
jungyoonn <sophia7625@naver.com>
2025-04-01 10:33:15 +0900

    슬라이더 사진 변경

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../../css/hero.css';
 import profileImg1 from '../../img/slider_profile_img2.png';
-import profileImg2 from '../../img/slider_profile_img3.png';
+import profileImg2 from '../../img/slider_profile_img9.png';
 import profileImg3 from '../../img/slider_profile_img1.png';
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Link } from 'react-router-dom';
